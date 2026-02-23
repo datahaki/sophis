1282253b689220374346dec87a116ebbe9de170a
datahaki <jan.hakenberg@gmail.com>
2026-02-23 17:38:03 +0100

    curve subdivision

--- src/main/java/ch/alpine/sophis/ref/d1/CurveSubdivision.java
+++ src/main/java/ch/alpine/sophis/ref/d1/CurveSubdivision.java
@@ -2,6 +2,7 @@
 package ch.alpine.sophis.ref.d1;
 
 import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.api.TensorUnaryOperator;
 
 public interface CurveSubdivision {
   /** @param tensor
@@ -12,7 +13,18 @@ public interface CurveSubdivision {
    * @return one round of subdivision of non-closed curve defined by given tensor */
   Tensor string(Tensor tensor);
 
+  /** @param tensor
+   * @param isCyclic
+   * @return */
   default Tensor auto(Tensor tensor, boolean isCyclic) {
     return isCyclic ? cyclic(tensor) : string(tensor);
+  }
+
+  /** @param isCyclic
+   * @return */
+  default TensorUnaryOperator auto(boolean isCyclic) {
+    return isCyclic //
+        ? this::cyclic
+        : this::string;
   }
 }
