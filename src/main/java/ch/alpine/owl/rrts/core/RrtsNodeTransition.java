ef276578d188d54ba2032d17ef97f841ce3024b9
datahaki <jan.hakenberg@gmail.com>
2026-02-25 08:04:14 +0100

    package extraction

--- src/main/java/ch/alpine/owl/rrts/core/RrtsNodeTransition.java
+++
@@ -1,8 +0,0 @@
-// code by jph
-package ch.alpine.owl.rrts.core;
-
-import ch.alpine.sophis.ts.Transition;
-
-public record RrtsNodeTransition(RrtsNode rrtsNode, Transition transition) {
-  // ---
-}
