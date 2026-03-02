03eb40e343fba7034e7c0ea41127b96a6b104220
datahaki <jan.hakenberg@gmail.com>
2026-03-02 23:33:22 +0100

    cross averaging test

--- src/test/java/ch/alpine/sophis/itp/CrossAveragingTest.java
+++ src/test/java/ch/alpine/sophis/itp/CrossAveragingTest.java
@@ -1,7 +1,6 @@
 // code by jph
 package ch.alpine.sophis.itp;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -15,7 +14,6 @@ import ch.alpine.sophus.hs.s.Sphere;
 import ch.alpine.sophus.lie.se2.Se2CoveringGroup;
 import ch.alpine.tensor.Tensor;
 import ch.alpine.tensor.alg.VectorQ;
-import ch.alpine.tensor.ext.Serialization;
 import ch.alpine.tensor.pdf.Distribution;
 import ch.alpine.tensor.pdf.RandomSample;
 import ch.alpine.tensor.pdf.RandomSampleInterface;
@@ -25,14 +23,13 @@ import ch.alpine.tensor.sca.var.InversePowerVariogram;
 
 class CrossAveragingTest {
   @Test
-  void testSimplePD() throws ClassNotFoundException, IOException {
+  void testSimplePD() {
     Map<Biinvariants, Biinvariant> map = Biinvariants.all(Se2CoveringGroup.INSTANCE);
     for (Biinvariant biinvariant : map.values()) {
       Distribution distribution = NormalDistribution.standard();
       int n = 4 + ThreadLocalRandom.current().nextInt(4);
       Tensor sequence = RandomVariate.of(distribution, n, 3);
-      Sedarim tensorUnaryOperator = Serialization.copy( //
-          biinvariant.weighting(InversePowerVariogram.of(2), sequence));
+      Sedarim tensorUnaryOperator = biinvariant.weighting(InversePowerVariogram.of(2), sequence);
       RandomSampleInterface randomSampleInterface = new Sphere(4);
       Tensor values = RandomSample.of(randomSampleInterface, n);
       Tensor point = RandomVariate.of(distribution, 3);
