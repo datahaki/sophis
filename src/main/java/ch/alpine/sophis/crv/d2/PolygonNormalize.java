6396b23b832c406065696a714945f3e0c5a6bd3a
datahaki <jan.hakenberg@gmail.com>
2026-03-04 06:54:21 +0100

    polygon normalize

--- src/main/java/ch/alpine/sophis/crv/d2/PolygonNormalize.java
+++ src/main/java/ch/alpine/sophis/crv/d2/PolygonNormalize.java
@@ -3,15 +3,23 @@ package ch.alpine.sophis.crv.d2;
 
 import ch.alpine.tensor.Scalar;
 import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.alg.Reverse;
+import ch.alpine.tensor.sca.Sign;
 import ch.alpine.tensor.sca.pow.Sqrt;
 
 public enum PolygonNormalize {
   ;
   /** @param polygon
-   * @param area
+   * @param area positive
    * @return */
   public static Tensor of(Tensor polygon, Scalar area) {
-    Scalar factor = Sqrt.FUNCTION.apply(area.divide(PolygonArea.of(polygon)));
+    Sign.requirePositive(area);
+    area = area.divide(PolygonArea.of(polygon));
+    if (Sign.isNegative(area)) {
+      polygon = Reverse.of(polygon);
+      area = area.negate();
+    }
+    Scalar factor = Sqrt.FUNCTION.apply(area);
     Tensor shift = PolygonCentroid.of(polygon).negate();
     return Tensor.of(polygon.stream().map(shift::add)).multiply(factor);
   }
